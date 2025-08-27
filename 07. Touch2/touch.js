74170df746f4eabfa3020f7931b10e0d717c4a06
carolrossi <anacarollinirossi@gmail.com>
2025-08-27 09:40:38 -0300

    fix: espaçamento

--- 07. Touch2/touch.js
+++ 07. Touch2/touch.js
@@ -24,38 +24,3 @@ setInterval(() => {
         digitalWrite(LED, STATUS)
     }
 }, 100)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// setInterval ( () {
-//     let count = count.read();
-//     console.log("TOUCH 1: ", count);
-// })
-// let LED = 2;
-
-// let LEDSTATUS = 0;
-
-
-// pinmode(LED, "output")
-
-// setInterval(function(){
-//     LEDSTATUS = !LEDSTATUS;
-//     digitalWrite(LED, LEDSTATUS)
-// }, 5000);
